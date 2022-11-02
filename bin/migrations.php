3ce00f86b78d06eaf9fc06a58ccc265181173dab
Micael Vinhas <dev.mvinhas@gmail.com>
2022-11-02 09:35:06 +0000

    Run migrations

---
+++ bin/migrations.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once '../config/env_loader.php';
+$args = [];
+if (isset($argc)) {
+    for ($i = 0; $i < $argc; $i++) {
+        if ($argv[$i] === '--populate') {
+            $args[] = 'populate';
+        } elseif ($argv[$i] === '--clean-tables') {
+            $args[] = 'cleanTables';
+        } elseif ($argv[$i] === '--drop-tables') {
+            $args[] = 'dropTables';
+        }
+    }
+}
+(new \Nifty\Migrations)->migrate($args);
